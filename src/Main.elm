a0632375a22e2c87d0708af2872c7885ebbeab3a
Nawa Manusitthipol <nawa@nawaman.net>
2019-01-21 20:02:18 -0500

    With elm

---
+++ src/Main.elm
@@ -0,0 +1,62 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    { count : Int
+    }
+
+
+initialModel : Model
+initialModel =
+    { count = 1
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+    | Reset
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
+
+        Decrement ->
+            { model
+                | count =
+                    if model.count <= 0 then
+                        0
+
+                    else
+                        model.count - 1
+            }
+
+        Reset ->
+            { model | count = 0 }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Increment ] [ text "Increse" ]
+        , div []
+            [ text <| String.fromInt model.count
+            , button [ onClick Reset ] [ text "Reset to zero" ]
+            ]
+        , button [ onClick Decrement ] [ text "Decrese" ]
+        ]
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
